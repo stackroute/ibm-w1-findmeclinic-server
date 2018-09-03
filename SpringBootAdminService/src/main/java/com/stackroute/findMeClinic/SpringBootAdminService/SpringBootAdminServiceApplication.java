b13e49dc1832ebdfc25fcfa3bd518cc36e804454
chandan <chandan.mishra@stackroute.in>
2018-09-03 16:03:51 +0530

    First commit

---
+++ SpringBootAdminService/src/main/java/com/stackroute/findMeClinic/SpringBootAdminService/SpringBootAdminServiceApplication.java
@@ -0,0 +1,12 @@
+package com.stackroute.findMeClinic.SpringBootAdminService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminServiceApplication.class, args);
+	}
+}
