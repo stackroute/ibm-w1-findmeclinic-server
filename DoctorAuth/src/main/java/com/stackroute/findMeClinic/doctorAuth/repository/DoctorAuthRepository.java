7a7f6f8e3627f4a094daee217d2e65f621a2c4b4
SanjanaElangovan <esanvi97@gmail.com>
2018-09-04 19:57:20 +0530

    Doctor Registeration controller

--- DoctorAuth/src/main/java/com/stackroute/findMeClinic/doctorAuth/repository/DoctorAuthRepository.java
+++ DoctorAuth/src/main/java/com/stackroute/findMeClinic/doctorAuth/repository/DoctorAuthRepository.java
@@ -1,5 +1,13 @@
 package com.stackroute.findMeClinic.doctorAuth.repository;
 
-public interface DoctorAuthRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.stackroute.findMeClinic.doctorAuth.model.Doctor;
+
+@Repository
+public interface DoctorAuthRepository extends JpaRepository<Doctor, String>{
+
+	public Doctor findDoctorByphoneNumber(String phoneNumber);
+	
 }
--- DoctorAuth/src/main/java/com/stackroute/findMeClinic/doctorAuth/service/DoctorAuthServiceImpl.java
+++ DoctorAuth/src/main/java/com/stackroute/findMeClinic/doctorAuth/service/DoctorAuthServiceImpl.java
@@ -1,5 +1,52 @@
 package com.stackroute.findMeClinic.doctorAuth.service;
 
-public class DoctorAuthServiceImpl {
+import java.util.NoSuchElementException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.stackroute.findMeClinic.doctorAuth.exception.DoctorAlreadyExistsEcxeption;
+import com.stackroute.findMeClinic.doctorAuth.model.Doctor;
+import com.stackroute.findMeClinic.doctorAuth.repository.DoctorAuthRepository;
+
+@Service
+public class DoctorAuthServiceImpl implements DoctorAuthService {
+
+	private DoctorAuthRepository doctorAuth;
+
+	@Autowired
+	public DoctorAuthServiceImpl(DoctorAuthRepository doctorAuth) {
+		this.doctorAuth = doctorAuth;
+	}
+
+	@Override
+	public boolean registerDoctor(Doctor doctor) throws DoctorAlreadyExistsEcxeption {
+		boolean flag = false;
+		String info = doctor.getEmail();
+
+		try {
+			if (!doctorAuth.existsById(info) && (findDoctorByphoneNumber(doctor.getPhoneNumber()) == null)) {
+				doctorAuth.save(doctor);
+				flag = true;
+			} else {
+				throw new DoctorAlreadyExistsEcxeption("Doctor Informatio already present");
+			}
+			if (flag == true) {
+				return flag;
+			} else {
+				return false;
+			}
+		} catch (NoSuchElementException exception) {
+			throw new DoctorAlreadyExistsEcxeption("Doctor Informatio already present");
+		}
+	}
+
+	public Doctor findDoctorByphoneNumber(String phoneNumber) {
+		Doctor info = doctorAuth.findDoctorByphoneNumber(phoneNumber);
+		if (info != null) {
+			return info;
+		} else {
+			return null;
+		}
+	}
 }
