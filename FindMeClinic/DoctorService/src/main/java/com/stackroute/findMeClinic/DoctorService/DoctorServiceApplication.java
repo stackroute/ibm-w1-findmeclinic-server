b80a3ddf64edadfd54d2f3e191356d34688a0037
jachri11 <jachri11@in.ibm.com>
2018-09-04 12:29:15 +0530

    First Commit

---
+++ FindMeClinic/DoctorService/src/main/java/com/stackroute/findMeClinic/DoctorService/DoctorServiceApplication.java
@@ -0,0 +1,12 @@
+package com.stackroute.findMeClinic.DoctorService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorServiceApplication.class, args);
+	}
+}
---
+++ FindMeClinic/PatientService/src/main/java/com/stackroute/findMeClinic/PatientService/PatientServiceApplication.java
@@ -0,0 +1,12 @@
+package com.stackroute.findMeClinic.PatientService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientServiceApplication.class, args);
+	}
+}
