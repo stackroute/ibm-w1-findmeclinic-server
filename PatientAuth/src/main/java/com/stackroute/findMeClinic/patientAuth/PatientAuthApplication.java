3486ca58ada1f04a986f196c47f500c05e4458cc
vsweth11 <vsweth11@in.ibm.com>
2018-09-04 13:00:26 +0530

    Added Patient Auth Service

---
+++ PatientAuth/src/main/java/com/stackroute/findMeClinic/patientAuth/PatientAuthApplication.java
@@ -0,0 +1,12 @@
+package com.stackroute.findMeClinic.patientAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientAuthApplication.class, args);
+	}
+}
