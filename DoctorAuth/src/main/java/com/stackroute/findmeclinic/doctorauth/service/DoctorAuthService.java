0039cb594181942c3bbcaf1f5f41e0abe5fdc211
SanjanaElangovan <40624859+SanjanaElangovan@users.noreply.github.com>
2018-09-06 16:12:48 +0530

    Update DoctorAuthService.java

--- DoctorAuth/src/main/java/com/stackroute/findmeclinic/doctorauth/service/DoctorAuthService.java
+++ DoctorAuth/src/main/java/com/stackroute/findmeclinic/doctorauth/service/DoctorAuthService.java
@@ -8,12 +8,10 @@ import com.stackroute.findmeclinic.doctorauth.model.Doctor;
 public interface DoctorAuthService {
 
 	
-<<<<<<< HEAD
+
 	public Doctor registerDoctor(Doctor doctor) throws DoctorAlreadyExistsEcxeption;
 	
-=======
-	public boolean registerDoctor(Doctor doctor) throws DoctorAlreadyExistsEcxeption;
->>>>>>> 842d0124078804c57292696bcccd6ce5af5b8450
+
 	public Doctor findDoctorBydoctorPhoneNumber(String doctorPhoneNumber);
 	public String loginToken(Doctor login) throws ServletException;
 	
