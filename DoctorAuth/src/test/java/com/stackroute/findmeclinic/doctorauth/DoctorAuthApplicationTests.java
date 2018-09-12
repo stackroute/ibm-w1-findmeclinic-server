8970a144a71c0e1d926acbc13aa625d221d32937
vsweth11 <vsweth11@in.ibm.com>
2018-09-12 13:59:19 +0530

    test changes

--- DoctorAuth/src/test/java/com/stackroute/findmeclinic/doctorauth/DoctorAuthApplicationTests.java
+++ DoctorAuth/src/test/java/com/stackroute/findmeclinic/doctorauth/DoctorAuthApplicationTests.java
@@ -1,7 +1,7 @@
 package com.stackroute.findmeclinic.doctorauth;
 
-/*import org.junit.Ignore;
-import org.junit.Test;*/
+import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -10,9 +10,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DoctorAuthApplicationTests {
 
-	/*@Test
+	@Test
 	@Ignore
 	public void contextLoads() {
 	}
-*/
+
 }
