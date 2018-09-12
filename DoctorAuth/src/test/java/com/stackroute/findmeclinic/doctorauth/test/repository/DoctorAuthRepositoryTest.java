8001cec70a417783444e3977ee0ca55342a8fff9
SanjanaElangovan <esanvi97@gmail.com>
2018-09-12 18:47:05 +0530

    changes made in doctorAuth test cases

--- DoctorAuth/src/test/java/com/stackroute/findmeclinic/doctorauth/test/repository/DoctorAuthRepositoryTest.java
+++ DoctorAuth/src/test/java/com/stackroute/findmeclinic/doctorauth/test/repository/DoctorAuthRepositoryTest.java
@@ -52,6 +52,7 @@ public class DoctorAuthRepositoryTest {
 		 Assert.assertEquals(doctor.getDoctorEmail(), fetchedDoctor.getDoctorEmail());
 	 }
 	 @Ignore
+	 @Test
 	 public void loginDoctorTest() {
          Doctor doctor1=   doctorAuthRepository.findDoctorByDoctorEmailAndDoctorPassword(doctor.getDoctorEmail(),doctor.getDoctorPassword());
          Assert.assertEquals(doctor.getDoctorEmail(),doctor1.getDoctorPassword());
