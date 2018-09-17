0f015585a40a124d0db8df672a69a92a63231019
Sivaranjani G <siva19191212@gmail.com>
2018-09-17 14:37:51 +0530

    PatientService added

---
+++ PatientService/src/main/java/com/stackroute/findmeclinic/patientservice/service/PatientService.java
@@ -0,0 +1,10 @@
+package com.stackroute.findmeclinic.patientservice.service;
+
+import com.stackroute.findmeclinic.patientservice.model.Patient;
+
+public interface PatientService {
+	
+	
+	public String addPatient(Patient patient); 
+
+}
