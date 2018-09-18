a94ccd43ff60d13201cf9e1f7ba8c7c019b19b00
Sivaranjani G <siva19191212@gmail.com>
2018-09-19 00:47:07 +0530

    added methods in service and controller of patientservice

--- PatientService/src/main/java/com/stackroute/findmeclinic/patientservice/service/PatientServiceImpl.java
+++ PatientService/src/main/java/com/stackroute/findmeclinic/patientservice/service/PatientServiceImpl.java
@@ -1,8 +1,12 @@
 package com.stackroute.findmeclinic.patientservice.service;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.stackroute.findmeclinic.patientservice.exception.PatientAlreadyExistsException;
 import com.stackroute.findmeclinic.patientservice.model.Patient;
 import com.stackroute.findmeclinic.patientservice.repository.PatientRepository;
 
@@ -14,11 +18,61 @@ public class PatientServiceImpl implements PatientService{
 	PatientRepository patientRepository ;
 
 	@Override
-	public String addPatient(Patient patient) {
-		patientRepository.save(patient);
-		return null;
-		
-		
+	public Patient addPatient(Patient patient) throws PatientAlreadyExistsException {
+
+		try {
+			if (!patientRepository.existsById(patient.getPatientEmail()))
+		 {
+				char[] firstNameArray = patient.getPatientFirstName().toCharArray();
+				firstNameArray[0] = Character.toUpperCase(firstNameArray[0]);
+				char[] lastNameArray = patient.getPatientLastName().toCharArray();
+				lastNameArray[0] = Character.toUpperCase(lastNameArray[0]);
+				patient.setPatientFirstName(String.valueOf(firstNameArray));
+				patient.setPatientLastName(String.valueOf(lastNameArray));
+				
+				patientRepository.insert(patient);
+
+				return patient;
+
+			} else {
+				throw new PatientAlreadyExistsException("patient already exist");
+			}
+
+		} catch (NoSuchElementException exception) {
+			throw new PatientAlreadyExistsException("Patient already exist");
+		}
+
 	}
+
+	@Override
+	public Patient getPatientByEmail(String emailId) {
+		Patient fetchedPatient = new Patient();
+		
+		if(patientRepository.existsById(emailId))
+		{
+			fetchedPatient=patientRepository.findById(emailId).get();
+		}
+		return fetchedPatient;
+			}
+
+	@Override
+	public String getPatientBadgeName(String emailId) {
+		Patient fetchedPatient = new Patient();
+		fetchedPatient=getPatientByEmail(emailId);
+		String firstName = fetchedPatient.getPatientFirstName();
+		String lastName = fetchedPatient.getPatientLastName();
+		String badgeName = firstName.substring(0,1).toUpperCase()+lastName.substring(0,1).toUpperCase();
+
+
+		return badgeName;
+	}
+
+	@Override
+	public Patient updatePatient(Patient patient) {
+		patientRepository.save(patient);
+		return patient;
+	}
+
+	
 
 }
