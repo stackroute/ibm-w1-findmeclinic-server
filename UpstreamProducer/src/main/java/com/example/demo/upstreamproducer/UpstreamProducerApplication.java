e20f6b5b9ba8f3b4be082fd94aa495288c70a24d
SanjanaElangovan <esanvi97@gmail.com>
2018-09-20 07:26:29 +0530

    Added upstreamProducer service

---
+++ UpstreamProducer/src/main/java/com/example/demo/upstreamproducer/UpstreamProducerApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo.upstreamproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpstreamProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpstreamProducerApplication.class, args);
+	}
+}
