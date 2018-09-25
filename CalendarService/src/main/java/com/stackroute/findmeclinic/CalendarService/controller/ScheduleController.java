2f5b8daea8bb08416a8c2ad1fb68b507f30c4535
Tanmaya <tansri11@in.ibm.com>
2018-09-25 18:45:37 +0530

    calendar service update

--- CalendarService/src/main/java/com/stackroute/findmeclinic/CalendarService/controller/ScheduleController.java
+++ CalendarService/src/main/java/com/stackroute/findmeclinic/CalendarService/controller/ScheduleController.java
@@ -1,5 +1,7 @@
 package com.stackroute.findmeclinic.CalendarService.controller;
 
+import com.stackroute.findmeclinic.CalendarService.exception.ScheduleAlreadyExistsException;
+import com.stackroute.findmeclinic.CalendarService.exception.ScheduleDoesNotExistException;
 import com.stackroute.findmeclinic.CalendarService.model.Schedule;
 import com.stackroute.findmeclinic.CalendarService.model.Slot;
 import com.stackroute.findmeclinic.CalendarService.service.ScheduleService;
@@ -23,57 +25,69 @@ public class ScheduleController {
 
     @PostMapping("/api/calendar/add")
     ResponseEntity<?> addSchedule(@RequestBody Schedule schedule) {
-        ResponseEntity<?> responseEntity=null;
-        Schedule newSchedule = scheduleService.createSchedule(schedule);
-        if(newSchedule!=null){
-            responseEntity= new ResponseEntity<>(newSchedule, HttpStatus.CREATED);
-        }
-        else{
-            responseEntity= new ResponseEntity<>(null, HttpStatus.CONFLICT);
+        ResponseEntity<?> responseEntity = null;
+        Schedule newSchedule;
+        try {
+            newSchedule = scheduleService.createSchedule(schedule);
+            if (newSchedule != null) {
+                responseEntity = new ResponseEntity<>(newSchedule, HttpStatus.CREATED);
+            }else {
+                responseEntity = new ResponseEntity<>("Empty", HttpStatus.CONFLICT);
+            }
+        } catch (ScheduleAlreadyExistsException e) {
+            responseEntity = new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
         }
         return responseEntity;
     }
 
 
     @DeleteMapping("/api/calendar/delete/{scheduleId}")
-    ResponseEntity<?> deleteSchedule(@PathVariable String scheduleId){
+    ResponseEntity<?> deleteSchedule(@PathVariable String scheduleId) {
         ResponseEntity<?> responseEntity = null;
-        if(scheduleService.deleteSchedule(scheduleId)){
-            responseEntity = new ResponseEntity<>("Deleted", HttpStatus.OK);
-        }else {
+        try {
+            if (scheduleService.deleteSchedule(scheduleId)) {
+                responseEntity = new ResponseEntity<>("Deleted", HttpStatus.OK);
+            } else {
+                responseEntity = new ResponseEntity<>("Not Found", HttpStatus.NOT_FOUND);
+            }
+        } catch (ScheduleDoesNotExistException e) {
             responseEntity = new ResponseEntity<>("Not Found", HttpStatus.NOT_FOUND);
         }
         return responseEntity;
     }
 
     @GetMapping("/api/calendar/get")
-    ResponseEntity<?> getSchedule(){
+    ResponseEntity<?> getSchedule() {
         ResponseEntity<?> responseEntity = null;
         List<Schedule> schedules = scheduleService.getAllSchedule();
-        responseEntity=new ResponseEntity<>(schedules, HttpStatus.OK);
+        responseEntity = new ResponseEntity<>(schedules, HttpStatus.OK);
         return responseEntity;
     }
 
     @GetMapping("/api/calendar/get/{docId}")
-    ResponseEntity<?> getScheduleByDoc(@PathVariable  String docId){
+    ResponseEntity<?> getScheduleByDoc(@PathVariable String docId) {
         ResponseEntity<?> responseEntity = null;
-        List<Schedule> schedulesDoc = scheduleService.getAllScheduleCreatedBy(docId);
-        if(schedulesDoc!=null) {
+        List<Schedule> schedulesDoc = null;
+        try {
+            schedulesDoc = scheduleService.getAllScheduleCreatedBy(docId);
+        } catch (ScheduleDoesNotExistException e) {
+            responseEntity = new ResponseEntity<>("Not Found", HttpStatus.NOT_FOUND);
+        }
+        if (schedulesDoc != null) {
             responseEntity = new ResponseEntity<>(schedulesDoc, HttpStatus.OK);
-        }else{
-            responseEntity= new ResponseEntity<>("Doctor Not Found", HttpStatus.NOT_FOUND);
+        } else {
+            responseEntity = new ResponseEntity<>("Doctor Not Found", HttpStatus.NOT_FOUND);
         }
         return responseEntity;
     }
-    
-    @GetMapping("/api/calendar/getSlots/{docId}")
-    ResponseEntity<?> getSlotsByDoc(@PathVariable  String docId){
-    	 ResponseEntity<?> responseEntity = null;
-         List<Slot> schedulesDoc = scheduleService.getAllSlotsCreatedBy(docId);
-         responseEntity = new ResponseEntity<>(schedulesDoc, HttpStatus.OK);
-         return responseEntity;
-    }
 
+    @GetMapping("/api/calendar/getSlots/{docId}")
+    ResponseEntity<?> getSlotsByDoc(@PathVariable String docId){
+        ResponseEntity<?> responseEntity = null;
+        List<Slot> schedulesDoc = scheduleService.getAllSlotsCreatedBy(docId);
+        responseEntity = new ResponseEntity<>(schedulesDoc, HttpStatus.OK);
+        return responseEntity;
+    }
 
 
 }
--- CalendarService/src/main/java/com/stackroute/findmeclinic/CalendarService/service/ScheduleService.java
+++ CalendarService/src/main/java/com/stackroute/findmeclinic/CalendarService/service/ScheduleService.java
@@ -1,5 +1,6 @@
 package com.stackroute.findmeclinic.CalendarService.service;
 
+import com.stackroute.findmeclinic.CalendarService.exception.ScheduleAlreadyExistsException;
 import com.stackroute.findmeclinic.CalendarService.exception.ScheduleDoesNotExistException;
 import com.stackroute.findmeclinic.CalendarService.model.Schedule;
 import com.stackroute.findmeclinic.CalendarService.model.Slot;
@@ -9,16 +10,9 @@ import java.util.List;
 import org.springframework.messaging.handler.annotation.Payload;
 
 public interface ScheduleService {
-   Schedule createSchedule(Schedule schedule);
+   Schedule createSchedule(Schedule schedule) throws ScheduleAlreadyExistsException;
     boolean deleteSchedule(String scheduleId) throws ScheduleDoesNotExistException;
     List<Schedule> getAllScheduleCreatedBy(String createdBy) throws ScheduleDoesNotExistException;
-
-    
-    public void post(Schedule schedule) ;
-    
-    public void listen(@Payload Schedule schedule);
-
     List<Schedule> getAllSchedule();
-    
     List<Slot> getAllSlotsCreatedBy(String createdBy) ;
 }
