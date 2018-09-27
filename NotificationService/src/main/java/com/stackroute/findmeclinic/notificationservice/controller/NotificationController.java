fa978cea89074b984288a15524ff9fce74a750bf
Priyanka Karthikeyan <40624860+PriyankaKarthikeyan@users.noreply.github.com>
2018-09-27 12:31:32 +0530

    Update NotificationController.java

--- NotificationService/src/main/java/com/stackroute/findmeclinic/notificationservice/controller/NotificationController.java
+++ NotificationService/src/main/java/com/stackroute/findmeclinic/notificationservice/controller/NotificationController.java
@@ -21,233 +21,80 @@ import com.stackroute.findmeclinic.notificationservice.service.Notificationservi
 @CrossOrigin("*")
 @RequestMapping("/api/v1/notify")
 public class NotificationController {
-<<<<<<< HEAD
-    /* @MessageMapping("/hello")
-        @SendTo("/topic/greetings")
-        public String sendNotification(String name) throws Exception {
+  /* @MessageMapping("/hello")
+      @SendTo("/topic/greetings")
+      public String sendNotification(String name) throws Exception {
 //            Thread.sleep(1000); // simulated delay
-         System.out.println("name is"+ name);
-         return name;
+       System.out.println("name is"+ name);
+       return name;
 //            return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
-        }*/
-    
-    
-    
-    private Notificationservice notificationService;
-    
-    @Autowired
-    public NotificationController(Notificationservice notificationService)
-    {
-        this.notificationService=notificationService;
-    }
-    
-    @PostMapping()
-    public ResponseEntity<?> getNotication( @RequestBody Notification notification){
-        System.out.println("notification object"+ notification);
-        System.out.println("priya");
-        ResponseEntity<?> responseEntity = null;
-        
-        if(notificationService.sendNotification(notification))
-        {
-         responseEntity = new ResponseEntity<>(notification,HttpStatus.OK);
-        }
-        else {
-            responseEntity = new ResponseEntity<>("cannot add", HttpStatus.CONFLICT);
-        }
-        return responseEntity;
-    }
-    
-    @GetMapping("/patient/{mail}")
-    
-    public ResponseEntity<?> getPatientNotication( @PathVariable String mail){
-        ResponseEntity<?> responseEntity = null;
-        List<Notification> patientNotification = notificationService.getPatientNotification(mail);
-        if(patientNotification!=null)
-        {
-             responseEntity = new ResponseEntity<>(patientNotification,HttpStatus.OK);
-=======
-<<<<<<< HEAD
-	/* @MessageMapping("/hello")
-	    @SendTo("/topic/greetings")
-	    public String sendNotification(String name) throws Exception {
-//	        Thread.sleep(1000); // simulated delay
-		 System.out.println("name is"+ name);
-		 return name;
-//	        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
-	    }*/
-	
-	
-	
-	private Notificationservice notificationService;
-	
-	@Autowired
-	public NotificationController(Notificationservice notificationService)
-	{
-		this.notificationService=notificationService;
-	}
-	
-	@PostMapping()
-	@ApiOperation("Posts the notification object from the BookingAppointmentService ")
-	public ResponseEntity<?> getNotication( @RequestBody Notification notification){
-		System.out.println("notification object"+ notification);
-		ResponseEntity<?> responseEntity = null;
-		
-		if(notificationService.sendNotification(notification))
-		{
-		 responseEntity = new ResponseEntity<>(notification,HttpStatus.OK);
-		}
-		else {
-			responseEntity = new ResponseEntity<>("cannot add", HttpStatus.CONFLICT);
-		}
-		return responseEntity;
-	}
-	
+      }*/
 
-	@ApiOperation("Fetching the patient details ")
-	@GetMapping("/patient/{mail}")
 
-	public ResponseEntity<?> getPatientNotication( @PathVariable String mail){
-		ResponseEntity<?> responseEntity = null;
-		List<Notification> patientNotification = notificationService.getPatientNotification(mail);
-		if(patientNotification!=null)
-		{
-			 responseEntity = new ResponseEntity<>(patientNotification,HttpStatus.OK);
->>>>>>> c1ad539b9645cb27e31f52947babf8615b7e60a1
 
-        }
-        else
-        {
-            responseEntity = new ResponseEntity<>("no prescription", HttpStatus.CONFLICT);
+  private Notificationservice notificationService;
 
-        }
-        
-        return responseEntity;
+  @Autowired
+  public NotificationController(Notificationservice notificationService)
+  {
+      this.notificationService=notificationService;
+  }
 
-<<<<<<< HEAD
-    }
-    
+  @PostMapping()
+  public ResponseEntity<?> getNotication( @RequestBody Notification notification){
+      System.out.println("notification object"+ notification);
+      System.out.println("priya");
+      ResponseEntity<?> responseEntity = null;
 
-    @GetMapping("/doctor/{mail}")    
-    public ResponseEntity<?> getDoctorNotication( @PathVariable String mail){
-        ResponseEntity<?> responseEntity = null;
-        List<Notification> patientNotification = notificationService.getDoctorNotification(mail);
-        if(patientNotification!=null)
-        {
-             responseEntity = new ResponseEntity<>(patientNotification,HttpStatus.OK);
+      if(notificationService.sendNotification(notification))
+      {
+       responseEntity = new ResponseEntity<>(notification,HttpStatus.OK);
+      }
+      else {
+          responseEntity = new ResponseEntity<>("cannot add", HttpStatus.CONFLICT);
+      }
+      return responseEntity;
+  }
 
-        }
-        else
-        {
-            responseEntity = new ResponseEntity<>("no prescription", HttpStatus.CONFLICT);
+  @GetMapping("/patient/{mail}")
 
-        }
-        
-        return responseEntity;
+  public ResponseEntity<?> getPatientNotication( @PathVariable String mail){
+      ResponseEntity<?> responseEntity = null;
+      List<Notification> patientNotification = notificationService.getPatientNotification(mail);
+      if(patientNotification!=null)
+      {
+           responseEntity = new ResponseEntity<>(patientNotification,HttpStatus.OK);
 
-    }
+      }
+      else
+      {
+          responseEntity = new ResponseEntity<>("no prescription", HttpStatus.CONFLICT);
+
+      }
+
+      return responseEntity;
+
+  }
+
+
+  @GetMapping("/doctor/{mail}")
+  public ResponseEntity<?> getDoctorNotication( @PathVariable String mail){
+      ResponseEntity<?> responseEntity = null;
+      List<Notification> patientNotification = notificationService.getDoctorNotification(mail);
+      if(patientNotification!=null)
+      {
+           responseEntity = new ResponseEntity<>(patientNotification,HttpStatus.OK);
+
+      }
+      else
+      {
+          responseEntity = new ResponseEntity<>("no prescription", HttpStatus.CONFLICT);
+
+      }
+
+      return responseEntity;
+
+  }
 
 
 }
-=======
-	}
-	
-    @ApiOperation("Fetching the doctor details")
-	@GetMapping("/doctor/{mail}")	
-	public ResponseEntity<?> getDoctorNotication( @PathVariable String mail){
-		ResponseEntity<?> responseEntity = null;
-		List<Notification> patientNotification = notificationService.getDoctorNotification(mail);
-		if(patientNotification!=null)
-		{
-			 responseEntity = new ResponseEntity<>(patientNotification,HttpStatus.OK);
-
-		}
-		else
-		{
-			responseEntity = new ResponseEntity<>("no prescription", HttpStatus.CONFLICT);
-
-		}
-		
-		return responseEntity;
-
-	}
-=======
-   /* @MessageMapping("/hello")
-       @SendTo("/topic/greetings")
-       public String sendNotification(String name) throws Exception {
-//            Thread.sleep(1000); // simulated delay
-        System.out.println("name is"+ name);
-        return name;
-//            return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
-       }*/
-
-
-
-   private Notificationservice notificationService;
-
-   @Autowired
-   public NotificationController(Notificationservice notificationService)
-   {
-       this.notificationService=notificationService;
-   }
-
-   @PostMapping()
-   public ResponseEntity<?> getNotication( @RequestBody Notification notification){
-       System.out.println("notification object"+ notification);
-       System.out.println("priya");
-       ResponseEntity<?> responseEntity = null;
-
-       if(notificationService.sendNotification(notification))
-       {
-        responseEntity = new ResponseEntity<>(notification,HttpStatus.OK);
-       }
-       else {
-           responseEntity = new ResponseEntity<>("cannot add", HttpStatus.CONFLICT);
-       }
-       return responseEntity;
-   }
-
-   @GetMapping("/patient/{mail}")
-
-   public ResponseEntity<?> getPatientNotication( @PathVariable String mail){
-       ResponseEntity<?> responseEntity = null;
-       List<Notification> patientNotification = notificationService.getPatientNotification(mail);
-       if(patientNotification!=null)
-       {
-            responseEntity = new ResponseEntity<>(patientNotification,HttpStatus.OK);
-
-       }
-       else
-       {
-           responseEntity = new ResponseEntity<>("no prescription", HttpStatus.CONFLICT);
-
-       }
-
-       return responseEntity;
-
-   }
-
-
-   @GetMapping("/doctor/{mail}")
-   public ResponseEntity<?> getDoctorNotication( @PathVariable String mail){
-       ResponseEntity<?> responseEntity = null;
-       List<Notification> patientNotification = notificationService.getDoctorNotification(mail);
-       if(patientNotification!=null)
-       {
-            responseEntity = new ResponseEntity<>(patientNotification,HttpStatus.OK);
-
-       }
-       else
-       {
-           responseEntity = new ResponseEntity<>("no prescription", HttpStatus.CONFLICT);
-
-       }
-
-       return responseEntity;
-
-   }
->>>>>>> 8b15f024334fc29b25eeaa57e8a491a658591610
-
-
-}
-
->>>>>>> c1ad539b9645cb27e31f52947babf8615b7e60a1
