1f6de0fc1784cf79260c20b9c693ce8261269663
Priyanka Karthikeyan <40624860+PriyankaKarthikeyan@users.noreply.github.com>
2018-09-27 12:42:19 +0530

    Update Notification.java

--- NotificationService/src/main/java/com/stackroute/findmeclinic/notificationservice/model/Notification.java
+++ NotificationService/src/main/java/com/stackroute/findmeclinic/notificationservice/model/Notification.java
@@ -8,13 +8,14 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document
 public class Notification {
 
-<<<<<<< HEAD
+
 	@Id
 	private String notification_id;
 	private String doctorId;
     private String patientId;
     private String content;
     private Date notifgenDate;
+	
     public String getDoctorId() {
 		return doctorId;
 	}
@@ -57,56 +58,4 @@ public class Notification {
 	
     
     
-    
-=======
-    @Id
-    private String notification_id;
-    private String doctorId;
-   private String patientId;
-   private String content;
-   private Date notifgenDate;
-   public String getDoctorId() {
-        return doctorId;
-    }
-    public void setDoctorId(String doctorId) {
-        this.doctorId = doctorId;
-    }
-    public String getPatientId() {
-        return patientId;
-    }
-    public void setPatientId(String patientId) {
-        this.patientId = patientId;
-    }
-    
-
-   public String getNotification_id() {
-       return notification_id;
-   }
-   public void setNotification_id(String notification_id) {
-       this.notification_id = notification_id;
-   }
-
-   public String getContent() {
-       return content;
-   }
-   public void setContent(String content) {
-       this.content = content;
-   }
-   public Date getNotifgenDate() {
-       return notifgenDate;
-   }
-   public void setNotifgenDate(Date notifgenDate) {
-       this.notifgenDate = notifgenDate;
-   }
-    @Override
-    public String toString() {
-        return "Notification [notification_id=" + notification_id + ", doctorId=" + doctorId + ", patientId="
-                + patientId + ", content=" + content + ", notifgenDate=" + notifgenDate + "]";
-        
-    }
-    
-
-
-
->>>>>>> c1ad539b9645cb27e31f52947babf8615b7e60a1
 }
